3dad91182f0b8dd522f6f1ed9e532e1daa8dd08d
Shoyayeb <sa.fine@Outlook.com>
2021-10-31 02:34:30 +0600

    get api added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
-const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +9,32 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4f4qc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tourplan");
+    const plansCollection = database.collection("plans");
+
+    // GET API
+    app.get("/plans", async (req, res) => {
+      const cursor = plansCollection.find({});
+      const plans = await cursor.toArray();
+      res.send(plans);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("server is perfectly running");
