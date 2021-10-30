4da7cf50ea4fe4e0a2aeafa5b3c0abe026deecbd
Shoyayeb <sa.fine@Outlook.com>
2021-10-31 02:19:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is perfectly running");
+});
+
+app.listen(port, () => {
+  console.log("Running server on", port);
+});
