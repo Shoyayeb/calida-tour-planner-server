89c04e79e24ea5b8ed07a3a19ab1254c0844b45c
Shoyayeb <sa.fine@Outlook.com>
2021-10-31 02:21:21 +0600

    mongodb config added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 const app = express();
